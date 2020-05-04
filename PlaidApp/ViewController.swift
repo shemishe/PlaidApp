08726673acab121a330c75a3e2813ab879d71846
Sherman Shi <shermanmishi@icloud.com>
2020-05-04 12:08:33 -0700

    Initial Commit

---
+++ PlaidApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PlaidApp
+//
+//  Created by Sherman Shi on 5/4/20.
+//  Copyright © 2020 Sherman Shi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
